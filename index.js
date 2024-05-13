a8c22a301aaedcab64c215d7490559f5d7143ed1
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-13 23:08:28 +0100

    add Assignment 02

--- index.js
+++ index.js
@@ -255,9 +255,18 @@ console.log(
 ); // eLZERO WEB SCHOOl // first and last index lowerCase, Dynamic solution
 //! Assignment 01
 let userName = "Elzero";
-console.log(userName.slice(0,1).toLowerCase()); // e
+console.log(userName.slice(0, 1).toLowerCase()); // e
 console.log(userName[0].toLowerCase()); // e
-console.log(userName.split("",1).toString().toLowerCase()); // e
-console.log(userName.substring(0,1).toLowerCase()); // e
-console.log(userName.substr(0,1).toLowerCase()); // e
+console.log(userName.split("", 1).toString().toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.substr(0, 1).toLowerCase()); // e
 console.log(userName.charAt(0).toLowerCase().repeat(3)); // eee
+//! Assignment 02
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
