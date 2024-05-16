fcfe6e0d202e544383cad7e2a1c72871ab8ee970
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-16 14:41:13 +0100

    Add Assignment 01

--- 40-47.js
+++ 40-47.js
@@ -15,3 +15,16 @@ let O = Elzero.slice(zero).toUpperCase();
 console.log(O);
 let rO = r + O;
 console.log(rO);
+let a = 1
+// ! Assignment 01
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+//* Method 1
+ console.log(myFriends.pop()); // ["Ahmed", "Elham", "Osama"];
+ console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+
+//* Method 2
+console.log(myFriends.push("jjj")); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.splice(3,1)); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
