3d8b2c29e81f1f21a647dfa009576ea356b0fc65
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-16 15:10:47 +0100

    Add Assignment 04

--- 40-47.js
+++ 40-47.js
@@ -8,33 +8,33 @@ let Elzero = `${my[--counter].slice(--zero, counter)}${my[--counter].substring(
 	++counter
 )}`;
 console.log(Elzero); // Elzero
-let r = Elzero.slice(counter + counter,--zero);
+let r = Elzero.slice(counter + counter, --zero);
 console.log(r);
 
 let O = Elzero.slice(zero).toUpperCase();
 console.log(O);
 let rO = r + O;
 console.log(rO);
-let a = 1
+let a = 1;
 // ! Assignment 01
 let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 //* Method 1
- console.log(myFriends.pop()); // ["Ahmed", "Elham", "Osama"];
- console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.pop()); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
 
 //* Method 2
 console.log(myFriends.push("jjj")); // ["Ahmed", "Elham", "Osama"];
-console.log(myFriends.splice(3,1)); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.splice(3, 1)); // ["Ahmed", "Elham", "Osama"];
 console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
 //! Assignment 02
 //$ No Slice method
 let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
 
 // Write Your Code Here
-console.log(friends.shift())
-console.log(friends.pop())
+console.log(friends.shift());
+console.log(friends.pop());
 console.log(friends); // ["Eman", "Osama"]
 //! Assignment 03
 //$ NO numbers, One line of code
@@ -43,5 +43,10 @@ let arrTwo = ["A", "B", "Z"];
 let finalArr = [];
 
 // Write One Single Line Of Code
-finalArr = arrTwo.pop().split().concat(arrOne.reverse(),arrTwo.reverse())
+finalArr = arrTwo.pop().split().concat(arrOne.reverse(), arrTwo.reverse());
 console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
+//! Assignment 04
+//$ NO numbers, only {0}; One line of code
+let website = "Go";
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+console.log(words[website.length][0].slice(website.length).toUpperCase()); // ZERO
