bf926945dfc3f406024f9b420831d708166738e5
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-17 21:01:54 +0100

    Update Assignment 07

--- for-loop-practice.js
+++ for-loop-practice.js
@@ -1,67 +1,67 @@
-// // ! Practice - Add Products To Page
-// let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
-// let colors = ["Red", "Green", "Black"];
-// let showCount = 5;
-// let models = [2020, 2021];
-// document.write(`<h1>Show ${showCount} Products.</h1>`);
-// for (let i = 0; i < showCount; i++) {
-// 	document.write(`<div>`);
-// 	document.write(`<h3>${i + 1}- ${products[i]}</h3>`);
-// 	for (let j = 0; j < colors.length; j++) {
-// 		// document.write(`<p>${colors[j]}</p>`)
-// 	}
-// 	document.write(`<p>${colors.join(" | ")}</p>`);
+// ! Practice - Add Products To Page
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
+let colors = ["Red", "Green", "Black"];
+let showCount = 5;
+let models = [2020, 2021];
+document.write(`<h1>Show ${showCount} Products.</h1>`);
+for (let i = 0; i < showCount; i++) {
+	document.write(`<div>`);
+	document.write(`<h3>${i + 1}- ${products[i]}</h3>`);
+	for (let j = 0; j < colors.length; j++) {
+		// document.write(`<p>${colors[j]}</p>`)
+	}
+	document.write(`<p>${colors.join(" | ")}</p>`);
 
-// 	document.write(`</div>`);
-// }
-// // ! Assignment 01
-// let start = 10;
-// let end = 100;
-// let exclude = 40;
-// for (let i = start; i <= end; i += start) {
-// 	if (i === exclude) continue;
-// 	console.log([i].join());
-// }
-// // Output
-// // 10 20 30 50 60 70 80 90  100
-// // ! Assignment 02
-// let startt = 10;
-// let endd = 0;
-// let stop = 3;
-// for (let j = startt; j >= endd; j--) {
-// 	j >= start ? console.log(j) : console.log(`${endd}${j}`);
-// 	if (j === stop) {
-// 		break;
-// 	}
-// }
-// // Output
-// // 10  09  08 07 06 05 04 03
-// // ! Assignment 03
-// let start_ = 1;
-// let end_ = 6;
-// let breaker = 2;
-// for (let h = start_; h <= end_; h++) {
-// 	console.log(h);
-// 	for (let x = breaker; x < end_; x += breaker) {
-// 		console.log("--", x);
-// 	}
-// }
-// // ! Assignment 04
-// let index = 10; //$ first condition
-// let jump = 2;
-// for (;;) {
-// 	// Write Your Code Here
-// 	console.log(index);
-// 	index -= jump; //$ third condition
-// 	if (index < jump + jump) break; //$ second condition
-// }
+	document.write(`</div>`);
+}
+// ! Assignment 01
+let start = 10;
+let end = 100;
+let exclude = 40;
+for (let i = start; i <= end; i += start) {
+	if (i === exclude) continue;
+	console.log([i].join());
+}
+// Output
+// 10 20 30 50 60 70 80 90  100
+// ! Assignment 02
+let startt = 10;
+let endd = 0;
+let stop = 3;
+for (let j = startt; j >= endd; j--) {
+	j >= start ? console.log(j) : console.log(`${endd}${j}`);
+	if (j === stop) {
+		break;
+	}
+}
+// Output
+// 10  09  08 07 06 05 04 03
+// ! Assignment 03
+let start_ = 1;
+let end_ = 6;
+let breaker = 2;
+for (let h = start_; h <= end_; h++) {
+	console.log(h);
+	for (let x = breaker; x < end_; x += breaker) {
+		console.log("--", x);
+	}
+}
+// ! Assignment 04
+let index = 10; //$ first condition
+let jump = 2;
+for (;;) {
+	// Write Your Code Here
+	console.log(index);
+	index -= jump; //$ third condition
+	if (index < jump + jump) break; //$ second condition
+}
 // //* Another solution
-// // $for (;;) {
-// // $  let i = index;
-// // $  console.log(i);
-// // $  index -= jump;
-// // $  if (index < 4) break;
-// // $}
+// $for (;;) {
+// $  let i = index;
+// $  console.log(i);
+// $  index -= jump;
+// $  if (index < 4) break;
+// $}
 // ! Assignment 05
 let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
 let letter = "a";
@@ -77,13 +77,13 @@ for (let q = letter.length - letter.length; q < friends.length; q++) {
 // "4 => Osama"
 // "5 => Sameh"
 // ! Assignment 06
-let start = 0;
+let startà = 0;
 let swappedName = "elZerO";
-
-for (let b = start; b < swappedName.length; b++) {
+let result = "";
+for (let b = startà; b < swappedName.length; b++) {
 	swappedName[b] === swappedName[b].toUpperCase()
-		? swappedName[b].toLowerCase()
-		: swappedName[b].toUpperCase();
+		? (result += swappedName[b].toLowerCase())
+		: (result += swappedName[b].toUpperCase());
 }
 console.log(result);
 // Output
@@ -92,10 +92,10 @@ console.log(result);
 let starting = 0;
 let mix = [1, 2, 3, "A", "B", "C", 4];
 //$ for (let k = -mix.indexOf(-("C")); k < mix.length; k++) {
-for (let k = -mix.lastIndexOf();k < mix.length; k++) {
-	if(typeof(mix[k])==="string") continue
+for (let k = -mix.lastIndexOf(); k < mix.length; k++) {
+	if (typeof mix[k] === "string") continue;
 	console.log(mix[k]);
-	}
+}
 
 // Output
 // 2;
