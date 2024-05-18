fcc301bb6f3015d62ba3cb84ad17659d2b11f642
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-18 13:41:12 +0100

    Add Assignment 01

--- loop.js
+++ loop.js
@@ -16,28 +16,43 @@ let myEmployees = [
 	"Omnya",
 ];
 let adminCount = 0;
-
-// Count the number of valid admins before displaying anything
 for (let i = 0; i < myAdmins.length; i++) {
 	if (myAdmins[i] === "Stop") {
 		break;
 	}
 	adminCount++;
 }
-// Display the number of admins at the top
-document.write(`<div>We have ${adminCount} Admins</div>`);
-// Display each admin and their corresponding team members
+document.write(`<div>We have ${adminCount} Admins`);
+
 for (let i = 0; i < myAdmins.length; i++) {
 	if (myAdmins[i] === "Stop") {
 		break;
 	}
-	document.write(`<hr><h4>The Admin for team ${i + 1} is ${myAdmins[i]}</h4>`);
-	document.write(`<h3>Team Members:</h3>`);
-	let memberCount = 0;
+	document.write(`<hr><h3>The Admin for Team ${i + 1} is ${myAdmins[i]}</h3>`);
+	document.write(`<h4>Team Members:</h4>`);
+	let employeeCount = 0;
 	for (let j = 0; j < myEmployees.length; j++) {
 		if (myEmployees[j][0] === myAdmins[i][0]) {
-			memberCount++;
-			document.write(`<p>${memberCount} - ${myEmployees[j]}</p>`);
+			employeeCount++;
+			document.write(`<p>${employeeCount} - ${myEmployees[j]}</p>`);
 		}
 	}
 }
+// ! Assignment 01
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let indexx = 0;
+let counter = 0;
+while (indexx < friends.length) {
+	if (
+		typeof friends[indexx] === "string" &&
+		!friends[indexx].startsWith("A") &&
+		!friends[indexx].startsWith("a")
+	) {
+		counter++;
+		document.write(`${counter} => ${friends[indexx]}<br>`);
+	}
+	indexx++;
+}
+// Output
+// "1 => Sayed"
+// "2 => Mahmoud"
---
+++ page.js
@@ -0,0 +1,26 @@
+let adminCount = 0;
+
+// Count the number of valid admins before displaying anything
+for (let i = 0; i < myAdmins.length; i++) {
+	if (myAdmins[i] === "Stop") {
+		break;
+	}
+	adminCount++;
+}
+// Display the number of admins at the top
+document.write(`<div>We have ${adminCount} Admins</div>`);
+// Display each admin and their corresponding team members
+for (let i = 0; i < myAdmins.length; i++) {
+	if (myAdmins[i] === "Stop") {
+		break;
+	}
+	document.write(`<hr><h4>The Admin for team ${i + 1} is ${myAdmins[i]}</h4>`);
+	document.write(`<h3>Team Members:</h3>`);
+	let memberCount = 0;
+	for (let j = 0; j < myEmployees.length; j++) {
+		if (myEmployees[j][0] === myAdmins[i][0]) {
+			memberCount++;
+			document.write(`<p>${memberCount} - ${myEmployees[j]}</p>`);
+		}
+	}
+}
