27700f1ba0f930915bea4a5803cc4a1ee0765e25
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-20 12:40:06 +0100

    Add Challenge 02

--- 64-70.js
+++ 64-70.js
@@ -1,23 +1,17 @@
-function parent() {
-	let a = 10;
-	function child() {
-		let a = 20;
-		console.log(a);
-		function grandChild() {
-			let a = 30;
-			console.log(a);
-		}
-		grandChild();
-	}
-	child();
-	console.log(a);
-}
-parent();
-
 //! Arrow function Challenge
-// function names(...name) {
-// 	return `String [${name.join("], " + "[")}] => Done!`;
-// }
+//$ Challenge 01
+//* function names(...name) {
+//* 	return `String [${name.join("], " + "[")}] => Done!`;
+//* }
 let names = (...name) => `String [${name.join("], " + "[")}] => Done!`;
 console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
 //$ Output: String [Osama], [Mohamed],[Ali],[Ibrahim] => Done!
+
+//? Challenge 02
+let myNumbers = [20, 50, 10, 60];
+//* let calc = (One, Two, ...nums) => One + Two + num[0];
+function calc(One, Two, ...nums) {
+	return One + Two + nums[0];
+}
+
+console.log(calc(10, 20, 50)); //? 80
