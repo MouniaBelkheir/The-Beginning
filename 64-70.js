4815c8f6eec9bb154342fc94159413e8b1ec0d02
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-20 20:19:33 +0100

    Add Assignment 01

--- 64-70.js
+++ 64-70.js
@@ -15,3 +15,39 @@ function calc(One, Two, ...nums) {
 }
 
 console.log(calc(10, 20, 50)); //? 80
+
+//! Functions and Scopes
+//! Assignment 01
+function getDetails(zName, zAge, zCountry) {
+	function namePattern(zName) {
+		let nPart = zName.split(" ");
+		let initials = nPart[1].charAt(0).toUpperCase();
+		return nPart[0] + " " + initials + ".";
+	}
+
+	function ageWithMessage(zAge) {
+		ages = zAge.split(" ");
+		return "Your age is " + ages[0];
+	}
+
+	function countryTwoLetters(zCountry) {
+		let cntry = zCountry.slice(0, 2);
+		cntry = cntry.toUpperCase();
+		return "You live in " + cntry;
+	}
+
+	function fullDetails() {
+		let name = namePattern(zName);
+		let age = ageWithMessage(zAge);
+		let country = countryTwoLetters(zCountry);
+		return `Hello ${name}, ${age}, ${country}`;
+	}
+	return fullDetails(); // Do Not Edit This
+}
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+//   //* Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+//   //* Hello Ahmed A., Your Age Is 32, You Live In SY
+// !!!!!!!!!!!!!!
