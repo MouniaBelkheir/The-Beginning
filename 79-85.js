4aac40657e9a3738891c8d4fc7d22fdb40f8fb52
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-24 17:36:44 +0100

    Dot Notation vs Bracket Notation

--- 79-85.js
+++ 79-85.js
@@ -1,8 +1,11 @@
 //! Object - Introduction
+let myVar = "country";
 let user = {
 	//? Properties
 	name: "Mounia",
 	age: 33,
+	"The city": "Biskra",
+	country: "Algeria",
 	//? Methods
 	sayHello: function () {
 		return "Hello";
@@ -13,3 +16,7 @@ console.log(user.name);
 console.log(user.age);
 console.log(user.sayHello());
 console.log(user.sayBye());
+//! Dot Notation vs Bracket Notation
+console.log(user["name"]);
+console.log(user["The city"]);
+console.log(user[myVar]);
