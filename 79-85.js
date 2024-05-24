fb04c59c8db001549997d73472ddf05783f072ed
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-24 18:16:13 +0100

    Nested Object And Advanced Trainings

--- 79-85.js
+++ 79-85.js
@@ -20,3 +20,40 @@ console.log(user.sayBye());
 console.log(user["name"]);
 console.log(user["The city"]);
 console.log(user[myVar]);
+//! Nested Object And Advanced Trainings
+let available = true;
+let theUser = {
+	name: "Mounia",
+	age: 33,
+	skills: ["HTML", "CSS", "JS"],
+	available: false,
+	adresses: {
+		USA: "New York",
+		CANADA: {
+			one: "Toronto",
+			two: "British Colombia",
+		},
+	},
+	checkAv: function () {
+		if (theUser.available === true) {
+			return "I am available";
+		} else {
+			return "I am not available";
+		}
+	},
+	check: () => (available === true ? "I am available" : "I am not available"),
+};
+console.log("*************");
+console.log(theUser.name);
+console.log(theUser.age);
+console.log(theUser.skills);
+console.log(theUser.available);
+console.log(theUser.adresses);
+console.log(theUser.adresses.USA);
+console.log(theUser.adresses.CANADA);
+console.log(theUser.adresses.CANADA.one);
+console.log(theUser.adresses.CANADA.two);
+console.log("*************");
+console.log(theUser["adresses"].CANADA["one"]);
+console.log(theUser.check());
+console.log(theUser.checkAv());
