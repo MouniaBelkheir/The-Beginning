61489f8460f3a39f7ae34f5e879b43bfcddcf729
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-25 19:03:41 +0100

    Create Object With Assign Method

--- 79-85.js
+++ 79-85.js
@@ -103,3 +103,30 @@ copyObj.age = 30;
 console.log(copyObj);
 console.log(copyObj.age);
 console.log(copyObj.doubleAge());
+//! Create Object With "Assign" Method
+let obj1 = {
+	prop1: 1,
+	meth1: function () {
+		return this.prop1;
+	},
+};
+console.log(obj1);
+let obj2 = {
+	prop2: 2,
+	meth2: function () {
+		return this.prop2;
+	},
+};
+
+let targetObject = {
+	prop1: 100,
+	prop3: 3,
+};
+let finalObject = Object.assign(targetObject, obj1, obj2);
+console.log(finalObject);
+
+finalObject.prop1 = 200;
+finalObject.prop4 = 4;
+console.log(finalObject);
+let newObject = Object.assign({}, obj1, { prop5: 5, prop6: 6 });
+console.log(newObject);
