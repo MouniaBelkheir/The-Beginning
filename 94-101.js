31455ec699f4bcd25df7c6faff49d4b04e85b296
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-06-18 17:15:28 +0100

    Event Simulation - Click Focus Blur

---
+++ 94-101.js
@@ -0,0 +1,28 @@
+// ! Event Simulation - Click Focus Blur
+window.onload = function () {
+	let one = document.querySelector(".one");
+	let two = document.querySelector(".two");
+    let submit = document.querySelector(".submit");
+	// تأكد من أن الحقل الأول يأخذ التركيز عند تحميل الصفحة
+	if (one) {
+		one.focus();
+	}
+	// إضافة حدث input للحقل الأول للتحقق من طول القيمة
+	one.addEventListener("input", function () {
+		// التأكد من أن القيمة تحتوي على رقم واحد فقط
+		if (one.value.length === 1) {
+			// نقل التركيز إلى الحقل الثاني
+			two.focus();
+		}
+	});
+	two.addEventListener("input", function () {
+		if (two.value.length === 1) {
+			submit.focus();
+		}
+	});
+};
+
+// submit.addEventListener("focus", function (event) {
+// 	document.links[0].click();
+// 	event.preventDefault();
+// });
