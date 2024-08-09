f1f3f72dd0f0972c2160c01e91c54c106d83e5df
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-08-09 13:54:00 +0100

    Lessons 102-110, Add Assignment 3

--- 102-110.js
+++ 102-110.js
@@ -24,25 +24,36 @@
 // 	document.body.appendChild(div);
 // }
 // ! Assignment 2
-setTimeout(popUp, 5000);
-function popUp() {
-	let div = document.createElement("div");
-	let h2 = document.createElement("h2");
-	let p = document.createElement("p");
-	document.body.prepend(div);
-	div.appendChild(h2);
-	div.appendChild(p);
-	h2.textContent = "Welcome";
-	p.textContent = "Welcome to Elzero Web school";
-	div.style.cssText =
-		"width:600px; display:flex; flex-direction:column; align-items:center;justify-content:center; background-color:#f5f3f4; position: absolute; top:50%; left:50%; transform:translate(-50% , -50%);border: 2px solid #e3e1e2; border-radius: 3px; font-family:Arial";
-	h2.style.cssText = "font-size:large; font-weight: bold;margin: 3px;";
-	p.style.cssText = "font-size:medium; margin: 5px; ";
-	let button = document.createElement("button");
-	button.setAttribute("type", "button");
-	button.textContent = "X";
-	div.appendChild(button);
-	button.style.cssText =
-		"width:45px; height:45px; font-size:24px; color:white; background-color: red; border:none; border-radius:50%; position:absolute; top:-20px; right:-20px; cursor:pointer";
-	button.addEventListener("click", () => div.remove());
+// setTimeout(popUp, 5000);
+// function popUp() {
+// 	let div = document.createElement("div");
+// 	let h2 = document.createElement("h2");
+// 	let p = document.createElement("p");
+// 	document.body.prepend(div);
+// 	div.appendChild(h2);
+// 	div.appendChild(p);
+// 	h2.textContent = "Welcome";
+// 	p.textContent = "Welcome to Elzero Web school";
+// 	div.style.cssText =
+// 		"width:600px; display:flex; flex-direction:column; align-items:center;justify-content:center; background-color:#f5f3f4; position: absolute; top:50%; left:50%; transform:translate(-50% , -50%);border: 2px solid #e3e1e2; border-radius: 3px; font-family:Arial";
+// 	h2.style.cssText = "font-size:large; font-weight: bold;margin: 3px;";
+// 	p.style.cssText = "font-size:medium; margin: 5px; ";
+// 	let button = document.createElement("button");
+// 	button.setAttribute("type", "button");
+// 	button.textContent = "X";
+// 	div.appendChild(button);
+// 	button.style.cssText =
+// 		"width:45px; height:45px; font-size:24px; color:white; background-color: red; border:none; border-radius:50%; position:absolute; top:-20px; right:-20px; cursor:pointer";
+// 	button.addEventListener("click", () => div.remove());
+// }
+// ! Assignment 3
+let div = document.createElement("div");
+document.body.prepend(div);
+div.textContent = 10;
+function count() {
+	div.innerHTML -= 1;
+	if (div.innerHTML === "0") {
+		clearInterval(counter);
+	}
 }
+let counter = setInterval(count, 1000);
